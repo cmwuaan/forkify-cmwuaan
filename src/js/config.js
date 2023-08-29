8eee99e6cd25cf8db5cb58f8f38fa2144085cffc
cmwuaan <minhquanrbpk@gmail.com>
2023-08-29 13:49:16 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const KEY = '46cd89a4-db56-411d-a954-2fdb2cafd653';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
