c9efcfb39f7f4a085ff6965ed30eea87325709b2
cmwuaan <minhquanrbpk@gmail.com>
2023-08-29 14:14:12 +0700

    Change API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const KEY = '46cd89a4-db56-411d-a954-2fdb2cafd653';
+export const KEY = '06209a00-7c24-42bf-bc73-f4b23364662f';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const MODAL_CLOSE_SEC = 2.5;
